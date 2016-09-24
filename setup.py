f59bc92f2a7aee444e70fa7787da8143b766ff1f
Antonis Katzourakis <antonis.katzourakis@gmail.com>
2016-09-24 02:55:37 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='pyopenbr',
-      version='0.1',
+      version='0.2',
       description='Improved Python Wrapper for the OpenBR Command-line Tool.',
       long_description=readme(),
       classifiers=[
